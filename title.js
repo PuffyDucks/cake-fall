8679bf2b2ed9c34e9d110a98d2b84a090896b291
cool9128 <9128cool@gmail.com>
2017-12-11 14:17:18 -0800

    title screen change

--- title.js
+++ title.js
@@ -1,7 +1,15 @@
 /* global game phaser game_state Phaser*/
 /* global localStorage */
 
-
+/*
+To Do:
+Add custom textures (unlocked at levels)
+Mouse clicking
+MOAR LEVELS
+shooting maybe
+keys to change mode, go through certain blocks
+music
+*/
 game_state.title = function () {};
 game_state.title.prototype = {
 
@@ -22,14 +30,17 @@ game_state.title.prototype = {
 
         this.selectedLevel = localStorage.level;
 
-        this.left = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-        // this.right = game.input.keyboard.addKey(Phaser.Keyboard.D);
+        this.enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 				this.down = game.input.keyboard.addKey(Phaser.Keyboard.S);
         this.up = game.input.keyboard.addKey(Phaser.Keyboard.W);
 				this.down2 = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
         this.up2 = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-        this.U = game.input.keyboard.addKey(Phaser.Keyboard.ZERO);
-        this.T = game.input.keyboard.addKey(Phaser.Keyboard.T);
+				this.left = game.input.keyboard.addKey(Phaser.Keyboard.A);
+        this.right = game.input.keyboard.addKey(Phaser.Keyboard.D);
+				this.left2 = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        this.right2 = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        this.zero = game.input.keyboard.addKey(Phaser.Keyboard.ZERO);
+        this.t = game.input.keyboard.addKey(Phaser.Keyboard.T);
 
         if (localStorage.skinOn !== "On" && localStorage.skinOn !== "Off") {
             localStorage.skinOn = "Off";
@@ -49,29 +60,29 @@ game_state.title.prototype = {
     },
 
     update: function() {
-        if (this.left.isDown) {
+        if (this.enter.isDown) {
             localStorage.playingLevel = this.selectedLevel;
             game.state.start('play');
         }
-        if (this.up.isDown || this.up2.isDown) {
+        if (this.up.isDown || this.up2.isDown || this.right.isDown || this.right2.isDown) {
             if (this.upWasDown === false) {
                 this.selectedLevel++;
                 this.upWasDown = true;
             }
         }
-        if (this.down.isDown || this.down2.isDown) {
+        if (this.down.isDown || this.down2.isDown || this.left.isDown || this.left2.isDown) {
             if (this.downWasDown === false) {
                 this.selectedLevel--;
                 this.downWasDown = true;
             }
         }
-        if (this.up.isUp && this.up2.isUp) {
+        if (this.up.isUp && this.up2.isUp && this.right.isUp && this.right2.isUp) {
             this.upWasDown = false;
         }
-        if (this.down.isUp && this.down2.isUp) {
+        if (this.down.isUp && this.down2.isUp && this.left.isUp && this.left2.isUp) {
             this.downWasDown = false;
         }
-        if (this.T.isDown && localStorage.finished === "true") {
+        if (this.t.isDown && localStorage.finished === "true") {
             if (this.TWasDown === false && localStorage.skinOn === "Off") {
                 localStorage.skinOn = "On";
                 this.TWasDown = true;
@@ -81,10 +92,10 @@ game_state.title.prototype = {
                 this.TWasDown = true;
             }
         }
-        if (this.T.isUp) {
+        if (this.t.isUp) {
             this.TWasDown = false;
         }
-        if (this.U.isDown && localStorage.finished === "true") {
+        if (this.u.isDown && localStorage.finished === "true") {
             localStorage.finished = false;
             localStorage.level = 1;
             localStorage.skinOn = false;
@@ -96,12 +107,12 @@ game_state.title.prototype = {
             this.selectedLevel = localStorage.level;
         }
 				if (localStorage.finished === "true") {
-            this.text.text = "Enter to start level "  + this.selectedLevel + "\nW/S or Up/Down arrow keys to change level \nPress 0 to reset the game. \nToggle Skin with T. \nChallenge Skin: " + localStorage.skinOn + "\n\nControls: \nA/D or Left/Right arrow keys to move \nDon't get hit \n\nMade by Louis";
+            this.text.text = "Press enter to start level "  + this.selectedLevel + "\nW/S or ↑↓ to change level \nPress 0 to reset the game. \nToggle Skin with T. \nChallenge Skin: " + localStorage.skinOn + "\n\nControls: \n←→ or Left/Right arrow keys to move \nDon't get hit \n\nMade by Louis";
             this.text.position.y = 75;
         } else {
-            this.text.text = "Enter to start level "  + this.selectedLevel + "\nW/S or Up/Down arrow keys to change level \n\nControls: \nA/D or Left/Right arrow keys to move \nDon't get hit \n\nMade by Louis";
-            this.text.position.x = 400 - this.text.width;
-            this.text.position.y = 300 - this.text.height;
+            this.text.text = "Press enter to start level "  + this.selectedLevel + "\nW/S or ↑↓ to change level \n\nControls: \nA/D or ←→ arrow keys to move \nDon't get hit \n\nMade by Louis";
+            this.text.position.x = 400 - this.text.width / 2;
+            this.text.position.y = 300 - this.text.height / 2;
         }
     },
 };
